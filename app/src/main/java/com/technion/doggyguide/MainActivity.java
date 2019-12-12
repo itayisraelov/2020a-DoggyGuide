d8ca51b6aae3961358b390c91bbb3db14873f5d2
asselqi <34541972+asselqi@users.noreply.github.com>
2019-12-12 05:06:53 +0200

    login screen GUI

--- app/src/main/java/com/technion/doggyguide/MainActivity.java
+++ app/src/main/java/com/technion/doggyguide/MainActivity.java
@@ -1,14 +1,34 @@
 package com.technion.doggyguide;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.technion.doggyguide.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity implements
+        DogOwnerConnectionFragment.OnFragmentInteractionListener,
+        OrganizationConnectionFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //do nothing
     }
 }
