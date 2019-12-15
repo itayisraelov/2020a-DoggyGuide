d5c55d64ae744fc39f998f996f580593ead08a29
itay israelov <fixxx@campus.technion.ac.il>
2019-12-15 10:17:31 +0200

    BUG_time_picker

--- app/src/main/java/com/technion/doggyguide/homeScreen/NotificationsFragment.java
+++ app/src/main/java/com/technion/doggyguide/homeScreen/NotificationsFragment.java
@@ -1,10 +1,6 @@
 package com.technion.doggyguide.homeScreen;
 
-import android.app.Notification;
-import android.app.PendingIntent;
 import android.app.TimePickerDialog;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,28 +10,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.technion.doggyguide.notifications.NotificationReceiver;
 import com.technion.doggyguide.R;
-import com.technion.doggyguide.homeActivity;
 import com.technion.doggyguide.notifications.TimePickerFragment;
 
-import static com.technion.doggyguide.app.App.CHANNEL_1_ID;
-import static com.technion.doggyguide.app.App.CHANNEL_2_ID;
 
 public class NotificationsFragment extends Fragment implements TimePickerDialog.OnTimeSetListener {
-
-    private NotificationManagerCompat notificationManager;
-    private EditText editTextTitle;
-    private EditText editTextMessage;
 
     @Nullable
     @Override
@@ -49,30 +34,8 @@ public class NotificationsFragment extends Fragment implements TimePickerDialog.
                 Toast.makeText(getActivity(), "fab clicked", Toast.LENGTH_SHORT).show();
             }
         });
-
-        Button btn_send_ch_1 = view.findViewById(R.id.send_channel1);
-        Button btn_send_ch_2 = view.findViewById(R.id.send_channel2);
-
-        notificationManager = NotificationManagerCompat.from(getActivity());
-
-        editTextTitle = view.findViewById(R.id.edit_text_title);
-        editTextMessage = view.findViewById(R.id.edit_text_message);
-
-        btn_send_ch_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendOnChannel1(v);
-            }
-        });
-
-        btn_send_ch_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendOnChannel2(v);
-            }
-        });
-
-        Button btn_time_picker = (Button) view.findViewById(R.id.time_picker);
+        
+        Button btn_time_picker = view.findViewById(R.id.time_picker);
         btn_time_picker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,49 +44,6 @@ public class NotificationsFragment extends Fragment implements TimePickerDialog.
             }
         });
         return view;
-    }
-
-    private void sendOnChannel1(View v) {
-        String title = editTextTitle.getText().toString();
-        String message = editTextMessage.getText().toString();
-
-        Intent activityIntent = new Intent(getActivity(), homeActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(getActivity(),
-                0, activityIntent, 0);
-
-        Intent broadcastIntent = new Intent(getActivity(), NotificationReceiver.class);
-        broadcastIntent.putExtra("toastMessage", message);
-        PendingIntent actionIntent = PendingIntent.getBroadcast(getActivity(),
-                0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Notification notification = new NotificationCompat.Builder(getActivity(), CHANNEL_1_ID)
-                .setSmallIcon(R.drawable.ic_one)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setColor(Color.BLUE)
-                .setContentIntent(contentIntent)
-                .setAutoCancel(true)
-                .setOnlyAlertOnce(true)
-                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
-                .build();
-
-        notificationManager.notify(1, notification);
-    }
-
-    private void sendOnChannel2(View v) {
-        String title = editTextTitle.getText().toString();
-        String message = editTextMessage.getText().toString();
-
-        Notification notification = new NotificationCompat.Builder(getActivity(), CHANNEL_2_ID)
-                .setSmallIcon(R.drawable.ic_two)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_LOW)
-                .build();
-
-        notificationManager.notify(2, notification);
     }
 
     @Override
