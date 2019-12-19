8576d02e60e1b52184254e5d0236848447e95bb3
itay israelov <fixxx@campus.technion.ac.il>
2019-12-19 19:16:19 +0200

    recycle view for alarm

---
+++ app/src/main/java/com/technion/doggyguide/homeScreen/alarm/walkAlarmActivity.java
@@ -0,0 +1,16 @@
+package com.technion.doggyguide.homeScreen.alarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.technion.doggyguide.R;
+
+public class walkAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
